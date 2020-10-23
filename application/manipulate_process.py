ac67354ae87fa72dd17f08d468c6de6aceef305d
khairajani <himanshukhairajani8@gmail.com>
2020-10-23 20:18:55 +0530

    -

--- application/manipulate_process.py
+++ application/manipulate_process.py
@@ -2,19 +2,20 @@
 import setproctitle
 import os
 import signal 
+import sys 
 
 def start_process(ps_name):
     setproctitle.setproctitle(ps_name)
-    i = 100000
+    i = 1000000
     j = 0
     while j<=i:
         print(j)
         j+=1
-   
-def stop_process(name): 
+
+def stop_process(ps_name): 
     try: 
         # iterating through each instance of the proess 
-        for line in os.popen("ps ax | grep " + name + " | grep -v grep"):  
+        for line in os.popen("ps ax | grep " + ps_name + " | grep -v grep"):  
             fields = line.split() 
 
             pid = fields[0]  
